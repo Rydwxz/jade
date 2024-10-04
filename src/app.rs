6ace832667e82789a347041bc52c7f08cd2c058f
Redward <collardmusic@gmail.com>
2024-10-03 20:43:17 -0700

    app.rs

---
+++ src/app.rs
@@ -0,0 +1,110 @@
+use ratatui::{
+    prelude::*,
+    buffer::Buffer,
+    crossterm::{
+        event, event::{
+            Event, KeyCode, KeyEvent, KeyEventKind}
+        ,
+    },
+    layout::{Constraint, Layout, Rect},
+    style::Color,
+    text::{Line, Span},
+    widgets::{Block, Tabs, Widget, List, ListItem, ListState},
+    Frame,
+};
+use std::{
+    io,
+    path::PathBuf,
+};
+
+use crate::jade::jfs::*;
+
+pub struct App {
+    running: bool,
+    cwd: Listing,
+    list: Vec<Listing>,
+    selection: usize,
+}
+impl App {
+    pub fn init() -> io::Result<Self>  {
+        let cwd = Listing::new(std::env::current_dir()?);
+
+        Ok(Self {
+            running: true,
+            list: list_dir(&cwd.path),
+            cwd,
+            selection: 0,
+        })
+    }
+
+    pub fn run_app<B: Backend>(&mut self, mut term: Terminal<B>) -> io::Result<bool> {
+        while self.running {
+            term.draw(|frame| self.draw_ui(frame))?;
+            self.handle_events()?;
+        }
+        Ok(true)
+    }
+
+    pub fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key) if key.kind == KeyEventKind::Press => self.handle_key_press(key),
+            _ => {}
+        }
+        Ok(())
+    }
+
+    pub fn handle_key_press(&mut self, key: KeyEvent) {
+        match key.code {
+            KeyCode::Esc | KeyCode::Char('q') => self.running = false,
+            _ => {},
+        }
+    }
+
+    pub fn draw_ui(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [dir_bar,
+        file_pane,
+        tool_pane,
+        com_bar] = Layout::vertical([
+            Constraint::Max(1),
+            Constraint::Percentage(60),
+            Constraint::Min(0),
+            Constraint::Min(1),
+        ]).areas(area);
+
+        self.draw_dirbar(dir_bar, buf);
+        self.draw_filepane(file_pane, buf);
+        self.draw_toolpane(tool_pane, buf);
+        self.draw_combar(com_bar, buf);
+    }
+}
+
+impl App {
+    fn draw_dirbar(&self, area: Rect, buf: &mut Buffer) {
+        Span::from(&self.cwd.name).render(area, buf);
+    }
+    fn draw_filepane(&self, area: Rect, buf: &mut Buffer) {
+        StatefulWidget::render(
+            List::new(
+                self.list.iter().map(
+                    |e| { ListItem::new(Span::from(&e.name)) }
+                )
+            ).highlight_style(Style::new().bg(Color::Rgb(255, 0, 0))),
+            area,
+            buf,
+            &mut ListState::default()
+            .with_selected(Some(self.selection))
+        );
+    }
+    fn draw_toolpane(&self, area: Rect, buf: &mut Buffer) {
+
+    }
+    fn draw_combar(&self, area: Rect, buf: &mut Buffer) {
+
+    }
+}
